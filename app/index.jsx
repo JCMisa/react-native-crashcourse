c1a98f9251287a00515fa9ec29b17a41c113621c
JC MISA <johncarlomisa399@gmail.com>
2024-08-03 18:11:34 +0800

    complaete the sign in page ui

--- app/index.jsx
+++ app/index.jsx
@@ -9,7 +9,7 @@ export default function Index() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full justify-center items-center min-h-[85vh] p-4">
+        <View className="w-full justify-center items-center min-h-[90vh] p-4">
           <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode="contain" />
           <Image source={images.cards} className="max-w-[380px] w-full h-[300px]" resizeMode="contain" />
 
