432add0a9186b7b8b8432a9926a01c16bc8abff6
JC MISA <johncarlomisa399@gmail.com>
2024-08-03 18:16:50 +0800

    complete the sign up page ui

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -21,7 +21,7 @@ const SignIn = () => {
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
         <Link href={'/'} className="text-white absolute top-10 left-0">Go Back</Link>
-        <View className="w-full justify-center min-h-[90vh] px-4 my-6">
+        <View className="w-full justify-center min-h-[95vh] px-4 my-6">
           <Image source={images.logo} resizeMethod="contain" className="w-[115px] h-[35px]" />
           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
 
@@ -51,7 +51,7 @@ const SignIn = () => {
             <Text className="text-lg text-gray-100 font-pregular">
               Don't have an account?
             </Text>
-            <Link href={'sign-up'} className="text-lg font-psemibold text-secondary-100">Sign Up</Link>
+            <Link href={'/sign-up'} className="text-lg font-psemibold text-secondary-100">Sign Up</Link>
           </View>
         </View>
       </ScrollView>
